8060d5c87218d4942d398f2276d902944e793a8d
alfar98001 <alfar98001@gmail.com>
2025-11-11 16:55:01 +0300

    1.4. Вызов метода с возвращаемым типом. [#167387]

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+
+        return "Неизвестное слово." + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dictionary = new DummyDic();
+        String word = dictionary.engToRus(" fruit.");
+        System.out.println(word);
+    }
+
+}
